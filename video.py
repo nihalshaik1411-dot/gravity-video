b6920857181d2f1d01b998af2869c919d5a5c717
nihalshaik1411-dot <nihalshaik1411@gmail.com>
2025-09-22 13:54:32 +0530

    Add files via upload

--- video.py
+++ video.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("📹 Video Player from GitHub")
 
 # Replace this URL with the raw GitHub link to your mp4 file
-video_url = "https://github.com/nihalshaik1411-dot/gravity-video/blob/main/gravity%20videeo.mp4"
+video_url = "https://gravity-video-mdjkhag628h23cavchhidd.streamlit.app/"
 
 st.markdown("### Playing video from GitHub repo:")
 st.video(video_url)
